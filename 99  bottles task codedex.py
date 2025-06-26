1b8b51d5a7ce03093f820faa4dd8214b4682663c
roslein <vernicagee953@gmail.com>
2025-06-26 20:39:19 +0300

    Update 99  bottles task codedex.py

--- 99  bottles task codedex.py
+++ 99  bottles task codedex.py
@@ -1,6 +1,6 @@
 i = 99
 for i in range (99, 0, -1): 
- print( f"{i} bottles of beer on the wall")
- print (f"{i} bottles of beer")
- print ("Take one down, pass it around")
- print (f"{i-1} bottles of beer on the wall")
+ print(""""{i} bottles of beer on the wall")
+ {i} bottles of beer")
+ Take one down, pass it around")
+ {i-1} bottles of beer on the wall")
